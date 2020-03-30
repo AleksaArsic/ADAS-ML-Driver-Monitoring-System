47376b494ca69d1bf51a1694c68f93b0faa39b0e
Aleksa Arsic <aleksa.arsic.ftn@gmail.com>
2020-03-30 10:20:55 +0200

    Python test app
    Python app for testing conda environment libraries added.

---
+++ Project/TestApp/TestApp/TestApp.py
@@ -0,0 +1,11 @@
+import numpy as np 
+import pandas as pd 
+import keras
+from sklearn.model_selection import train_test_split
+import cv2
+
+import os
+from PIL import Image
+import PIL as PIL
+
+import tensorflow as tf 
