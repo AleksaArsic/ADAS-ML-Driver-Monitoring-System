631849c138cd29d4bc2c0fce274c0010f8d40607
Aleksa Arsic <aleksa.arsic.ftn@gmail.com>
2020-05-20 17:07:04 +0200

    Image capture script resolution update
    Image capture script now implements functionality for standardizing capture resolution to desired one

--- Project/ImageCapture/ImageCapture/ImageCapture.py
+++ Project/ImageCapture/ImageCapture/ImageCapture.py
@@ -5,6 +5,12 @@ import cv2
 import datetime
 import numpy as np
 
+ARG_ERR = 255
+
+# resolution of output captures
+captureWidth = 640
+captureHeight = 480
+
 windowName = "Video source"
 startTrackbarName = "Capture (STOP/START):"
 keyframesTrackbarName = "Capture FPS: "
@@ -19,6 +25,8 @@ workingDirPath = os.path.dirname(os.path.realpath(__file__))
 outputDirCount = 0
 
 vSourceHelp = "Video source (0 - Internal camera, 1 - External camera, <path>\*.mp4)"
+outputWidthHelp = "Width of the output capture"
+outputHeightHelp = "Height of the output capture"
 
 def startCapture(val):
     if(val == 1):
@@ -29,21 +37,51 @@ def startCapture(val):
 def keyframesTracker(val):
     pass
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
 def change_res(cap, width, height):
     cap.set(3, width)
     cap.set(4, height)
 
 def frameCapture(vsource):
 
+    resizeVideoFrame = False
+
     if(isinstance(vsource, int)):
         cap = cv2.VideoCapture(vsource + cv2.CAP_DSHOW)
-        change_res(cap, 640, 480)
+        change_res(cap, captureWidth, captureHeight)
     else:
         cap = cv2.VideoCapture(vsource)
-
-
-    #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        resizeVideoFrame = True
 
     if(cap.isOpened() == False):
         print("Error opening video source")
@@ -76,6 +114,9 @@ def frameCapture(vsource):
 
         ret, frame = cap.read();
         
+        if (resizeVideoFrame):
+            frame = image_resize(frame, captureWidth, captureHeight)
+
         # frames of interest
         keyframesPerSec = keyframesTrackbarMax - cv2.getTrackbarPos(keyframesTrackbarName, windowName)
 
@@ -111,23 +152,55 @@ def createNewOutputDir(dateTimeNow):
 
     return outputDirName
 
-def main():
+def argParser():
+    global captureWidth
+    global captureHeight
 
-    videoSource = ""
+    argsOK = True
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--vsource", help = vSourceHelp, required = True)
+    parser.add_argument("-ow", "--out_width", help = outputWidthHelp, required = False)
+    parser.add_argument("-oh", "--out_height", help = outputHeightHelp, required = False)
 
     args = vars(parser.parse_args())
 
-    if (args["vsource"] == '0' or args["vsource"] == '1'):
-        videoSource = int(args["vsource"])
-    elif (len(args["vsource"]) > 1):
-        videoSource = args["vsource"]
-    else:
-        print("Usage: ImageCapture.py -v <videosource>")
-        sys.exit(2)
+    try:
+        if (args["vsource"] == '0' or args["vsource"] == '1'):
+            videoSource = int(args["vsource"])
+        elif (len(args["vsource"]) > 1):
+            videoSource = args["vsource"]
+        else:
+            argsOK = False;
 
+        if args["out_width"] is not None or args["out_height"] is not None:
+            argsOK = True
+            #else:
+            if(len(args["out_width"])):
+                captureWidth = int(args["out_width"])
+            else:
+                argsOK = False
+            if(len(args["out_height"])):
+                captureHeight = int(args["out_height"])
+            else:
+                argsOK = False
+
+        if not argsOK:
+            print("Usage: ImageCapture.py -v <videosource> [-ow <out_width>, -oh <out_height>]")
+            sys.exit(ARG_ERR)
+
+    except SystemExit as e:
+        if e.code != ARG_ERR:
+            raise
+        else:
+            os._exit(ARG_ERR)
+
+    return videoSource
+
+def main():
+
+    videoSource = argParser()
+   
     print("Video source: ", videoSource)
 
     print(workingDirPath)
