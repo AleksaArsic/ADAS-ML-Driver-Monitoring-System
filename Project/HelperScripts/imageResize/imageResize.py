998789e7ac66f8961c94912bca05a6c2887995ee
Aleksa Arsic <aleksa.arsic.ftn@gmail.com>
2020-06-30 13:23:45 +0200

    data set csv files
    data set csv files modified to train/test data sets

---
+++ Project/HelperScripts/imageResize/imageResize.py
@@ -0,0 +1,43 @@
+import cv2
+
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+	
+def main():
+
+	img = cv2.imread('WIN_20200520_14_23_28_Pro.jpg', cv2.IMREAD_GRAYSCALE)
+
+	img = image_resize(img, 640, 480)
+	cv2.imshow('image2',img)
+	cv2.waitKey(0)
+	
+if __name__ == "__main__":
+	main()
