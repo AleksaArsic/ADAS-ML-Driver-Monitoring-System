95bbf47b5512127687d28b02c8f2459eb1e1a746
Aleksa Arsic <aleksa.arsic@rt-rk.com>
2021-04-16 17:00:39 +0200

    labelConverter.py

--- Project/HelperScripts/labelConverter/labelConverter.py
+++ Project/HelperScripts/labelConverter/labelConverter.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 # paths of input files
-inputCSVPath_phase01 = 'C:\\Users\\Cisra\\Desktop\\Rad\\trainingSet_phase01_csv\\trainingSet_phase01.csv'
-inputCSVPath_phase02 = 'C:\\Users\\Cisra\\Desktop\\Rad\\trainingSet_phase02_csv\\trainingSet_phase02.csv'
-inputCSVPath_phase03 = 'C:\\Users\\Cisra\\Desktop\\Rad\\trainingSet_phase03_csv\\trainingSet_phase03.csv'
+inputCSVPath_phase01 = r'C:\Users\arsic\Desktop\master\Rad\CNN-Driver-Monitoring-System\Dataset\trainingSet_phase01_csv\trainingSet_phase01.csv'
+inputCSVPath_phase02 = r'C:\Users\arsic\Desktop\master\Rad\CNN-Driver-Monitoring-System\Dataset\trainingSet_phase02_csv\trainingSet_phase02.csv'
+inputCSVPath_phase03 = r'C:\Users\arsic\Desktop\master\Rad\CNN-Driver-Monitoring-System\Dataset\trainingSet_phase03_csv\trainingSet_phase03.csv'
 
 # last item of interest in phase 3 labels
 cPh03LastItem = 12
@@ -155,6 +155,7 @@ def translatePhaseThreeToPhaseOne(commonLabels):
         topERightX, topERightY = eyeCropPoints(commonLabels[i][1][cRightEyeX], commonLabels[i][1][cRightEyeY], faceW)[0]
  
         # calculate face elements coordinates on face image
+        # in respect to whole image
         for k in range(3, len(commonLabels[i][1]) - 1, 2):
             commonLabels[i][1][k] += topLeftX
             commonLabels[i][1][k + 1] += topLeftY
@@ -168,6 +169,8 @@ def translatePhaseThreeToPhaseOne(commonLabels):
             commonLabels[i][3][k] += (commonLabels[i][1][3] + topERightX)
             commonLabels[i][3][k + 1] += (commonLabels[i][1][4] + topERightY)
 
+# x - coordinate of eye centre
+# y - coordinate of eye centre
 def eyeCropPoints(x, y, faceW):
     # calculate coordinates to crop from
     height, width = faceW * cFaceWidthHeightRatio, faceW
@@ -209,9 +212,9 @@ if __name__ == "__main__":
     datasetCastToFloat(phase03_labels)
 
     common_labels = []
-    #common_labels = findCommonLabels(phase01_labels, phase02_labels, phase03_labels)
+    common_labels = findCommonLabels(phase01_labels, phase02_labels, phase03_labels)
 
-    common_labels = [[['capture_2020_04_17_11_39_49_7263', 0.0, 350.0, 331.0, 154.0], ['capture_2020_04_17_11_39_49_7263', 0.0, 0.0, 52.0, 141.0, 144.0, 139.0, 99.0, 173.0, 97.0, 216.0, 97.0, 230.0, 0.0, 0.0, 0.0, 0.0, 200.0], ['capture_2020_04_17_11_39_49_7263_left', 0.0, 50.0, 40.0, 51.0, 56.0, 51.0, 73.0, 18.0, 61.0, 88.0, 56.0], ['capture_2020_04_17_11_39_49_7263_right', 0.0, 54.0, 36.0, 53.0, 51.0, 56.0, 70.0, 24.0, 53.0, 93.0, 55.0]]]
+    #common_labels = [[['capture_2020_04_17_11_39_49_7263', 0.0, 350.0, 331.0, 154.0], ['capture_2020_04_17_11_39_49_7263', 0.0, 0.0, 52.0, 141.0, 144.0, 139.0, 99.0, 173.0, 97.0, 216.0, 97.0, 230.0, 0.0, 0.0, 0.0, 0.0, 200.0], ['capture_2020_04_17_11_39_49_7263_left', 0.0, 50.0, 40.0, 51.0, 56.0, 51.0, 73.0, 18.0, 61.0, 88.0, 56.0], ['capture_2020_04_17_11_39_49_7263_right', 0.0, 54.0, 36.0, 53.0, 51.0, 56.0, 70.0, 24.0, 53.0, 93.0, 55.0]]]
 
     print(common_labels)
 
@@ -219,4 +222,4 @@ if __name__ == "__main__":
     translatePhaseThreeToPhaseOne(common_labels)
 
     # save output labels
-    saveCSV('C:\\Users\\Cisra\\Desktop\\Rad\\output.csv', common_labels)
+    saveCSV(r'C:\Users\arsic\Desktop\master\Rad\output.csv', common_labels)
